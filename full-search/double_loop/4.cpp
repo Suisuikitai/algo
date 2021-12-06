2939ef5d0258fbbd9cc794475ece36d936dbe1df
Suisuikitai <mag.cupw@gmail.com>
2021-12-07 01:56:57 +0900

    double

--- full-search/double_loop/4.cpp
+++ full-search/double_loop/4.cpp
@@ -5,5 +5,14 @@ int main(int argc, char const *argv[])
 {
 	string S;
 	cin >> S;
+	map<char, int> m;
+	for (char i = 'a'; i <= 'z'; i++)
+		m[i] = 0;
+	for (int i = 0; i < S.length(); i++)
+		m[S[i]] = 1;
+	int ans = 0;
+	for (char i = 'a'; i <= 'z'; i++)
+		ans += m[i];
+	cout << ans << endl;
 	return 0;
 }
