b801faed3fed6e1bd43ad043e8ac8923df4e2192
Suisuikitai <mag.cupw@gmail.com>
2021-12-07 03:09:41 +0900

    multi_array

---
+++ full-search/multi_array/1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int N, M;
+	cin >> N >> M;
+	int A[N];
+	for (int i = 0; i < N; i++)
+		cin >> A[i];
+	int B[M];
+	for (int i = 0; i < M; i++)
+		cin >> B[i];
+	int count = 0;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			if (A[i] > B[j])
+				count++;
+	cout << count << endl;
+	return 0;
+}
