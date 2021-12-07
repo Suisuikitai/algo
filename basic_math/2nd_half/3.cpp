728568e4645c6d68adb5c9d479d1bdc6869a6c4a
Suisuikitai <mag.cupw@gmail.com>
2021-12-08 00:30:17 +0900

    basic_math

---
+++ basic_math/2nd_half/3.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int L, A;
+	cin >> L >> A;
+	cout << L / A << endl;
+	return 0;
+}
---
+++ basic_math/2nd_half/4.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int N, A;
+	cin >> N >> A;
+	cout << (N + A - 1) / A << endl;
+	return 0;
+}
