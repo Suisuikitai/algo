1ce0f4ce70a58cdf50fae1b882ec306b2da195c3
Suisuikitai <mag.cupw@gmail.com>
2021-12-16 00:24:41 +0900

    regex

---
+++ regular_expression/meta_1/1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <regex>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	string S;
+	cin >> S;
+	regex reg{R"(algo)"};
+	smatch m;
+	if (regex_search(S, m, reg))
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+	return 0;
+}
