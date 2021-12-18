5b166f1bc695b9a7e40c4ce5cf19764abc070bbb
Suisuikitai <mag.cupw@gmail.com>
2021-12-18 23:34:41 +0900

    reg

--- regular_expression/look_ahead_behind/4.cpp
+++ regular_expression/look_ahead_behind/4.cpp
@@ -8,10 +8,23 @@ int main(int argc, char const *argv[])
 	string Y, M;
 	cin >> N >> Y >> M;
 	string S;
-	regex reg{R"(\d+_.+_)"};
+	smatch m;
+	string regs;
+	regex reg{R"(([^_]+_|\d{2}\.pdf$))"};
+	cout << N << Y << M << endl;
+	//年月に当たる部分のみを抜き出して比較し、
+	//一致している場合は領収書名を抜き出す
 	for (int i = 0; i < N; i++)
 	{
 		cin >> S;
+		string yearmonth = regex_replace(S, reg, "");
+		if (yearmonth == Y + M)
+		{
+			cout << "Yes" << endl;
+		}
+		// regex reg{R"(_\d{8}\.pdf$)"};
+		// string tmp = regex_replace(S, reg, "");
+		// cout << tmp << endl;
 	}
 	return 0;
 }
