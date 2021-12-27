e73d72f97509a90ab1bec3da3c5c98bdf1c5fb1c
Suisuikitai <mag.cupw@gmail.com>
2021-12-27 22:42:05 +0900

    dp

---
+++ dynamic_programming/basic_dp/6.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <regex>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int N, M;
+	cin >> N >> M;
+	vector<int> D(M, 0);
+	for (int &d : D)
+		cin >> d;
+	vector<bool> dp(N + 1, false);
+	dp[0] = true;
+	for (int i = 1; i < N + 1; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			if (i - D[j] >= 0 && dp[i - D[j]])
+			{
+				dp[i] = dp[i - D[j]];
+				break;
+			}
+		}
+	}
+	if (dp[N])
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+	return 0;
+}
