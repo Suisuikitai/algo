55c7d6d7a8630b1ddefaa282c63b19bca914f9e6
Suisuikitai <mag.cupw@gmail.com>
2022-01-03 19:24:09 +0900

    part_sum

--- dynamic_programming/part_sum/1.cpp
+++ dynamic_programming/part_sum/1.cpp
@@ -15,13 +15,13 @@ int main(int argc, char const *argv[])
 	dp[0][0] = 1;
 	for (int i = 0; i < N - 1; i++)
 	{
-		for (int j = 0; j < M - A[i]; j++)
+		for (int j = 0; j < M; j++)
 		{
-			if (dp[i][j])
-			{
-				dp[i + 1][j] = 1;
+			if (!dp[i][j])
+				continue;
+			dp[i + 1][j] = 1;
+			if (j + A[i] < M)
 				dp[i + 1][j + A[i]] = 1;
-			}
 		}
 	}
 	int ans = 0;
