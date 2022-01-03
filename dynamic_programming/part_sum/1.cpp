1753f4ea72f0999dff6dd5b8fb9aaac3aa10535b
Suisuikitai <mag.cupw@gmail.com>
2022-01-03 19:09:46 +0900

    part_sum

---
+++ dynamic_programming/part_sum/1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int N, M;
+	cin >> N >> M;
+	int A[N - 1];
+	for (int i = 0; i < N - 1; i++)
+		cin >> A[i];
+	int dp[N][M];
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			dp[i][j] = 0;
+	dp[0][0] = 1;
+	for (int i = 0; i < N - 1; i++)
+	{
+		for (int j = 0; j < M - A[i]; j++)
+		{
+			if (dp[i][j])
+			{
+				dp[i + 1][j] = 1;
+				dp[i + 1][j + A[i]] = 1;
+			}
+		}
+	}
+	int ans = 0;
+	for (int i = 0; i < M; i++)
+		if (dp[N - 1][i])
+			ans++;
+	cout << ans << endl;
+	return 0;
+}
