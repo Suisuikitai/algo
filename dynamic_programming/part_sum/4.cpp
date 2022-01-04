9ef9932cc6abe83e00a7599ae68cc4d549413b9c
Suisuikitai <mag.cupw@gmail.com>
2022-01-05 00:45:21 +0900

    なんか違う

---
+++ dynamic_programming/part_sum/4.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int N, M;
+	cin >> N >> M;
+	int W[N], V[N];
+	for (int i = 0; i < N; i++)
+		cin >> W[i];
+	for (int i = 0; i < N; i++)
+		cin >> V[i];
+	int dp[N][M + 1];
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M + 1; j++)
+			dp[i][j] = 0;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M + 1; j++)
+		{
+			if (i - 1 < 0)
+				continue;
+			dp[i][j] = dp[i - 1][j];
+			if (j - W[i] >= 0)
+				dp[i][j] = max(dp[i][j - W[i]] + V[i], dp[i][j]);
+		}
+	cout << dp[N - 1][M] << endl;
+	return 0;
+}
