75f8fb07cdc44015e320e0342fe67a409f441a9b
Suisuikitai <mag.cupw@gmail.com>
2022-01-05 23:42:38 +0900

    模範と違う正答

--- dynamic_programming/part_sum/4.cpp
+++ dynamic_programming/part_sum/4.cpp
@@ -10,19 +10,19 @@ int main(int argc, char const *argv[])
 		cin >> W[i];
 	for (int i = 0; i < N; i++)
 		cin >> V[i];
-	int dp[N][M + 1];
-	for (int i = 0; i < N; i++)
+	int dp[N + 1][M + 1];
+	for (int i = 0; i < N + 1; i++)
 		for (int j = 0; j < M + 1; j++)
 			dp[i][j] = 0;
 	for (int i = 0; i < N; i++)
+	{
 		for (int j = 0; j < M + 1; j++)
 		{
-			if (i - 1 < 0)
-				continue;
-			dp[i][j] = dp[i - 1][j];
+			dp[i + 1][j] = max(dp[i][j], dp[i + 1][j]);
 			if (j - W[i] >= 0)
-				dp[i][j] = max(dp[i][j - W[i]] + V[i], dp[i][j]);
+				dp[i + 1][j] = max(dp[i][j - W[i]] + V[i], dp[i + 1][j]);
 		}
-	cout << dp[N - 1][M] << endl;
+	}
+	cout << dp[N][M] << endl;
 	return 0;
 }
