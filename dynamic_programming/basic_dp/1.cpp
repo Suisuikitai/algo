7e3b2ae40cf82c0b4bc6d7321ce1283329fae575
Suisuikitai <mag.cupw@gmail.com>
2022-01-11 21:39:13 +0900

    recursive

--- dynamic_programming/basic_dp/1.cpp
+++ dynamic_programming/basic_dp/1.cpp
@@ -2,15 +2,17 @@
 #include <regex>
 using namespace std;
 
+int solve(int A0, int A1, int rec)
+{
+	if (rec == 1)
+		return A0;
+	return solve(A1, (A0 + A1) % 100, rec - 1);
+}
+
 int main(int argc, char const *argv[])
 {
 	int N, X, Y;
 	cin >> N >> X >> Y;
-	vector<int> A(N, 0);
-	A[0] = X;
-	A[1] = Y;
-	for (int i = 2; i < N; i++)
-		A[i] = (A[i - 2] + A[i - 1]) % 100;
-	cout << A[N - 1] << endl;
+	cout << solve(X, Y, N) << endl;
 	return 0;
 }
