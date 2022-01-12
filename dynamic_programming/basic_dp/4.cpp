d158024511ef250bbd3c2301986069ebef355930
Suisuikitai <mag.cupw@gmail.com>
2022-01-12 21:50:29 +0900

    recursive

--- dynamic_programming/basic_dp/4.cpp
+++ dynamic_programming/basic_dp/4.cpp
@@ -2,17 +2,22 @@
 #include <regex>
 using namespace std;
 
+int solve(int a1, int a2, int a3, int N)
+{
+	if (N == 3)
+		return a3;
+	return solve(a2, a3, a3 + a2 + a1, N - 1);
+}
+
 int main(int argc, char const *argv[])
 {
 	int N;
 	cin >> N;
-	vector<int> A(31, 0);
-	A[0] = 0;
-	A[1] = 1;
-	A[2] = 2;
-	A[3] = 4;
-	for (int i = 4; i <= N; i++)
-		A[i] = A[i - 1] + A[i - 2] + A[i - 3];
-	cout << A[N] << endl;
+	if (N == 1)
+		cout << 1 << endl;
+	else if (N == 2)
+		cout << 2 << endl;
+	else
+		cout << solve(1, 2, 4, N) << endl;
 	return 0;
 }
