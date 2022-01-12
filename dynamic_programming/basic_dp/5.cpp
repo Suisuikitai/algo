589dad83a0ff0fa6f3aabf773c3c289ac5bac76e
Suisuikitai <mag.cupw@gmail.com>
2022-01-12 22:49:42 +0900

    basic

--- dynamic_programming/basic_dp/5.cpp
+++ dynamic_programming/basic_dp/5.cpp
@@ -9,18 +9,15 @@ int main(int argc, char const *argv[])
 	vector<int> A(N, 0);
 	for (int &a : A)
 		cin >> a;
-	vector<int> T(N, 0);
-	T[0] = 0;
+	vector<int> dp(N, 0);
+	dp[0] = 0;
 	for (int i = 1; i < N; i++)
 	{
-		T[i] = T[i - 1] + A[i];
-		for (int j = 1; j <= M; j++)
-		{
-			if (i - j < 0)
-				break;
-			T[i] = min(T[i], T[i - j] + A[i] * j);
-		}
+		dp[i] = A[i] + dp[i - 1];
+		for (int j = 2; j < M + 1; j++)
+			if (i - j >= 0)
+				dp[i] = min(dp[i], j * A[i] + dp[i - j]);
 	}
-	cout << T[N - 1] << endl;
+	cout << dp[N - 1] << endl;
 	return 0;
 }
