f3c86167bf491472ddd61d16d656f52288263f9a
Suisuikitai <mag.cupw@gmail.com>
2022-01-22 00:35:32 +0900

    one more

--- dynamic_programming/part_sum/7_2.cpp
+++ dynamic_programming/part_sum/7_2.cpp
@@ -3,16 +3,6 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	int N = 0;
-	cin >> N;
-	vector<int> W(N);
-	int size = 0;
-	for (int &w : W)
-	{
-		cin >> w;
-		size += w;
-	}
-	// dpで格納する値をどう使うか考える必要がある
-	//部分問題として何を求めることができるか
+	int N;
 	return 0;
 }
