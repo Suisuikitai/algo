ff1200b49f47b895a16f9737771f0ec220b5cd4e
Suisuikitai <mag.cupw@gmail.com>
2022-02-12 19:57:32 +0900

    ac

--- dynamic_programming/knapsack/10.cpp
+++ dynamic_programming/knapsack/10.cpp
@@ -1,10 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define INF 10000000
 
 int main(int argc, char const *argv[])
 {
 	string S, T;
 	cin >> S >> T;
-	//変更、削除、挿入の全てを試して最もコストが小さかったものを採用する
+	vector<vector<int>> dp(S.length() + 1, vector<int>(T.length() + 1, INF));
+	for (int i = 0; i < S.length() + 1; i++)
+		dp[i][0] = i;
+	for (int j = 1; j < T.length() + 1; j++)
+		dp[0][j] = j;
+	for (int i = 0; i < S.length(); i++)
+	{
+		for (int j = 0; j < T.length(); j++)
+		{
+			if (S[i] == T[j])
+				dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[i][j]);
+			else
+				dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[i][j] + 1);
+			dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[i][j + 1] + 1);
+			dp[i + 1][j + 1] = min(dp[i + 1][j + 1], dp[i + 1][j] + 1);
+		}
+	}
+	cout << dp[S.length()][T.length()] << endl;
 	return 0;
 }
