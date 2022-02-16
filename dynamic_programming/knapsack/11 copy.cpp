3b8eec68c003650d403a36e15ae0ce45dd6576b7
Suisuikitai <mag.cupw@gmail.com>
2022-02-17 01:43:50 +0900

    ac

--- dynamic_programming/knapsack/11 copy.cpp
+++ dynamic_programming/knapsack/11 copy.cpp
@@ -10,10 +10,8 @@ int main(int argc, char const *argv[])
 		for (int j = 0; j < T; j++)
 			cin >> g[i][j];
 
-	vector<int> dp(110, 0);
-	// 0~Tを1始まりにしたら1~T+1になった
+	vector<int> dp(T + 2, 0);
 	for (int t = 1; t <= T + 1; t++)
-		// iはtの手前まで増加させる。漸化式で使用する
 		for (int i = 0; i < t; i++)
 			for (int j = i + 1; j < t; j++)
 				dp[t] = max(dp[t], dp[i] + g[i][j - 1]);
