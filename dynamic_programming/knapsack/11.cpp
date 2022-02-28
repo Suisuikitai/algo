dc612f1db64fef9937e089df054fd2e081c7c47b
Suisuikitai <mag.cupw@gmail.com>
2022-02-28 21:36:21 +0900

    ac

--- dynamic_programming/knapsack/11.cpp
+++ dynamic_programming/knapsack/11.cpp
@@ -10,10 +10,13 @@ int main(int argc, char const *argv[])
 		for (int j = 0; j < T; j++)
 			cin >> g[i][j];
 	vector<int> dp(T + 2, 0);
-	for (int t = 1; t < T + 2; t++)
+	for (int t = 1; t < T + 2; T++)
+	{
 		for (int i = 0; i < t; i++)
+		{
 			for (int j = i + 1; j < t; j++)
-				dp[t] = max(dp[t], dp[i] + g[i][j - 1]);
-	cout << dp[T + 1] << endl;
+				dp[t] = max(dp[t], dp[t - 1] + g[i][j - 1]);
+		}
+	}
 	return 0;
 }
