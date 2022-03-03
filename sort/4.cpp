af7016010798c1beba9057afaea9ebfa047f7ef6
Suisuikitai <mag.cupw@gmail.com>
2022-03-03 23:36:40 +0900

    comment

---
+++ sort/4.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int N;
+	cin >> N;
+	vector<int> A(N);
+	for (int &a : A)
+		cin >> a;
+	//アルゴリズムはイメージを図や表にしてかくといいかも
+	return 0;
+}
