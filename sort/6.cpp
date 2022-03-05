cb3780eea3e697e439fb8d20a50560b947c5030a
Suisuikitai <mag.cupw@gmail.com>
2022-03-06 02:47:12 +0900

    comment

---
+++ sort/6.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//図に起こす
+vector<int> mergesort(vector<int> A)
+{
+}
+
+int main(int argc, char const *argv[])
+{
+	int N;
+	cin >> N;
+	vector<int> A(N);
+	for (int &a : A)
+		cin >> a;
+	return 0;
+}
