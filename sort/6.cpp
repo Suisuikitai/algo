192e5d81367d0d1b3eb23f4d49363b39ef0aea4e
Suisuikitai <mag.cupw@gmail.com>
2022-03-07 21:40:36 +0900

    ac

--- sort/6.cpp
+++ sort/6.cpp
@@ -1,9 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//図に起こす
-vector<int> mergesort(vector<int> A)
+void mergesort(vector<int> &a, int left, int right)
 {
+	if (right - left < 2)
+		return;
+	int mid = (right - left) / 2;
+	mergesort(a, left, mid);
+	mergesort(a, mid, right);
+
+	//ここの工夫まじでうまい
+	vector<int> buf;
+	for (int i = left; i < mid; i++)
+		buf.push_back(a[i]);
+	for (int i = right - 1; i >= mid; i--)
+		buf.push_back(a[i]);
+
+	int iterator_left = 0;
+	int iterator_right = (int)buf.size() - 1;
+	for (int i = left; i < right; ++i)
+	{
+		if (buf[iterator_left] <= buf[iterator_right])
+			a[i] = buf[iterator_left++];
+		else
+			a[i] = buf[iterator_right--];
+	}
 }
 
 int main(int argc, char const *argv[])
@@ -13,5 +34,6 @@ int main(int argc, char const *argv[])
 	vector<int> A(N);
 	for (int &a : A)
 		cin >> a;
+	mergesort(A, 0, N);
 	return 0;
 }
