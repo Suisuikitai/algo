1f167cf9d587310045e50a9b515eb9bd2e59ace2
Suisuikitai <mag.cupw@gmail.com>
2022-03-17 12:06:28 +0900

    比較関数の書き方とラムダ式

--- sort/apply/6.cpp
+++ sort/apply/6.cpp
@@ -5,5 +5,23 @@ int main(int argc, char const *argv[])
 {
 	int N;
 	cin >> N;
+	vector<int> vote(N, 0);
+	int a;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> a;
+		vote[a]++;
+	}
+	vector<int> student(N);
+	for (int i = 0; i < N; i++)
+		student[i] = i;
+
+	auto cmp = [&](int x, int y) -> bool
+	{
+		return vote[x] > vote[y];
+	};
+	stable_sort(student.begin(), student.end(), cmp);
+	for (int i : student)
+		cout << i << " " << vote[i] << endl;
 	return 0;
 }
